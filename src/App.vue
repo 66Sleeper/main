643788debdb46111c9ebea8814b00ce97f0e36ed
66Sleeper <1149619184@qq.com>
2021-03-03 18:58:11 +0800

    3

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <div><p style="color: #2285ff"> 这里是主模块 </p></div>
     <Project1 />
+    <HelloWorld />
   </div>
 </template>
 
 <script>
 import Project1 from '@/lib/src/components/Project1'
+import HelloWorld from '@/submodule2/src/components/HelloWorld'
 
 export default {
   name: 'App',
   components: {
     Project1,
+    HelloWorld,
   }
 }
 </script>
